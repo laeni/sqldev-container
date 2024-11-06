a5af8ca22906d8a2b807fe9457637b48fed18d22
root <root@localhost.localdomain>
2024-11-06 13:53:53 +0800

    fix: 修复无法构建 Oracle 客户端基础镜像的问题

--- client/oracle/build-21.14.0.0.0dbru.sh
+++ client/oracle/build-21.14.0.0.0dbru.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+cd $(dirname $(readlink -f "$0"))/../..
+
 # https://www.oracle.com/cn/database/technologies/instant-client/linux-x86-64-downloads.html
 # Docker 无法识别直接从 oracle 官网下载的 zip 包，所以导致 ADD 会将整个未解压的文件添加进去，
 # 所以需要先提前解压，然后使用 COPY 命令直接拷贝解压后的文件即可
 
 ORACLE_FILE=instantclient-basiclite-linux.x64-21.14.0.0.0dbru.zip
 
-../../download-file.sh $ORACLE_FILE "9447acb812afdbe274d6eff083738640" https://download.oracle.com/otn_software/linux/instantclient/2114000/$ORACLE_FILE
+./download-file.sh $ORACLE_FILE "9447acb812afdbe274d6eff083738640" https://download.oracle.com/otn_software/linux/instantclient/2114000/$ORACLE_FILE
 
 rm -rf instantclient META-INF
 unzip $ORACLE_FILE
 mv instantclient_21_14 instantclient
 
-docker build -t oracle-client-basiclite:21.14.0.0.0dbru -f Dockerfile-oracle-client .
+docker build -t ccr.ccs.tencentyun.com/laeni/oracle-client-basiclite:21.14.0.0.0dbru -f Dockerfile-oracle-client .
--- client/oracle/build-23.4.0.24.05.sh
+++ client/oracle/build-23.4.0.24.05.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+cd $(dirname $(readlink -f "$0"))/../..
+
 # https://www.oracle.com/cn/database/technologies/instant-client/linux-x86-64-downloads.html
 # Docker 无法识别直接从 oracle 官网下载的 zip 包，所以导致 ADD 会将整个未解压的文件添加进去，
 # 所以需要先提前解压，然后使用 COPY 命令直接拷贝解压后的文件即可
 
 ORACLE_FILE=instantclient-basiclite-linux.x64-23.4.0.24.05.zip
 
-../../download-file.sh $ORACLE_FILE "afab029f7b5c7e1c74956d6a52b00598" https://download.oracle.com/otn_software/linux/instantclient/2340000/$ORACLE_FILE
+./download-file.sh $ORACLE_FILE "afab029f7b5c7e1c74956d6a52b00598" https://download.oracle.com/otn_software/linux/instantclient/2340000/$ORACLE_FILE
 
 rm -rf instantclient META-INF
 unzip $ORACLE_FILE
 mv instantclient_23_4 instantclient
 
-docker build -t oracle-client-basiclite:23.4.0.24.05 -f Dockerfile-oracle-client .
+docker build -t ccr.ccs.tencentyun.com/laeni/oracle-client-basiclite:23.4.0.24.05 -f Dockerfile-oracle-client .
