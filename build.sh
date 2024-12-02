1513a4078655ec403433a4cca5cdf87cf4318bd8
chendaiwang <chendaiwang@iboxpay.com>
2024-12-02 09:28:47 +0800

    perf: 优化构建脚本

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+SQLDEV_VERSION=2.2.78
 SQLDEV_FILE=sqldev-free-2.2.78-20241129065720.tar.gz
+SQLDEV_FILE_MD5=ad10af1e21b21879dee608dc0c159fa3 # md5 获取: curl -sSL https://s1.cloud.shuaninfo.com/free/$SQLDEV_FILE | md5sum
 
-# md5 获取: curl -sSL https://s1.cloud.shuaninfo.com/free/$SQLDEV_FILE | md5sum
-./download-file.sh $SQLDEV_FILE "ad10af1e21b21879dee608dc0c159fa3" https://s1.cloud.shuaninfo.com/free/$SQLDEV_FILE
+./download-file.sh $SQLDEV_FILE $SQLDEV_FILE_MD5 https://s1.cloud.shuaninfo.com/free/$SQLDEV_FILE
 
 rm -rf sqldev
 tar -xf $SQLDEV_FILE
 
-docker build -t ccr.ccs.tencentyun.com/laeni/sqldev:2.2.78 -f Dockerfile .
+docker build -t laeni/sqldev:$SQLDEV_VERSION -f Dockerfile .
+docker tag laeni/sqldev:$SQLDEV_VERSION ccr.ccs.tencentyun.com/laeni/sqldev:$SQLDEV_VERSION
